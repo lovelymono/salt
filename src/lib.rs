faf6939fb2c612325a746e0e183f41246441d005
Lena Milizé <me@lvmn.org>
2022-04-17 17:08:06 +0200

    refactor: remove unnecessary assertions
    Signed-off-by: Lena Milizé <me@lvmn.org>

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,7 @@ mod tests {
 	fn bigint_works() {
 		let mut a = Integer::from(0xff);
 		assert_eq!(a.to_string_radix(16), "ff");
-		a = a << 96;
-		assert_eq!(a.to_string_radix(16), "ff000000000000000000000000");
-		a += 0xee;
+		a = (a << 96) + 0xee;
 		assert_eq!(a.to_string_radix(16), "ff0000000000000000000000ee");
 	}
 }
