032078bf4887a29fdba4677e44ff1eec2f59783f
Lena Milizé <me@lvmn.org>
2022-04-20 22:48:23 +0200

    doc: use svg instead of png for logo in docs
    Signed-off-by: Lena Milizé <me@lvmn.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_logo_url = "https://raw.githubusercontent.com/lovelymono/salt/default/assets/salt-clear.png")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/lovelymono/salt/default/assets/salt-clear.svg")]
 
 #[cfg(test)]
 mod tests {
